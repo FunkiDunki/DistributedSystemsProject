c83c5f127bf0c17c2106e6417d4d186aa42e0057
Justin Kopcinski <jkopcin@gmail.comn>
2025-06-04 15:06:48 -0700

    Added the BuildFeatures.scala program. Had to change the build.sbt file to different scala version for the code to run (please revert if necessary)

--- ScalaCode/build.sbt
+++ ScalaCode/build.sbt
@@ -2,12 +2,13 @@ name := "ScalaCode"
 
 version := "0.1"
 
-scalaVersion := "2.11.8"
+scalaVersion := "2.12.15"
 
-val sparkVersion = "2.4.8"
+val sparkVersion = "3.2.0"
 
 resolvers ++= Seq(
-  "apache-snapshots" at "https://repository.apache.org/snapshots/"
+  "apache-snapshots" at "https://repository.apache.org/snapshots/",
+  "maven-central" at "https://repo1.maven.org/maven2/"
 )
 
 libraryDependencies ++= Seq(
@@ -15,5 +16,10 @@ libraryDependencies ++= Seq(
   "org.apache.spark" %% "spark-sql" % sparkVersion,
   "org.apache.spark" %% "spark-mllib" % sparkVersion,
   "org.apache.spark" %% "spark-streaming" % sparkVersion,
-  "org.apache.spark" %% "spark-hive" % sparkVersion
+  "org.apache.spark" %% "spark-hive" % sparkVersion,
+  "org.scala-lang" % "scala-reflect" % scalaVersion.value
 )
+
+// Add these settings to ensure proper dependency resolution
+fork in run := true
+javaOptions ++= Seq("-Xms512M", "-Xmx2048M", "-XX:+CMSClassUnloadingEnabled")
--- ScalaCode/src/main/scala/BuildFeatures.scala
+++ ScalaCode/src/main/scala/BuildFeatures.scala
@@ -1,43 +1,71 @@
-import org.apache.spark.sql.{SparkSession, functions => F}
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.{lpad, col}
 import org.apache.spark.sql.types.DoubleType
 
 object BuildFeatures {
     def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder()
       .appName("Build Features for KMeans")
-      .master("local[*]") // Use all available cores
+      .master("local[*]")
       .getOrCreate()
 
+    //Spark 2.4.8, we need to import this way
     import spark.implicits._
 
     val targetNaics = "------" //change this value if specific naics requested
 
     val businessDF = spark.read
         .option("header", "true")
-        .csv("..Data/zipcodeBusinessPatterns.txt")
-        .withColumn("zip", F.lpad($"zip", 5, "0"))
-        .withColumn("est", $"est".cast(DoubleType))
-        .filter($"naics" === targetNaics)
+        .csv("../Data/zipcodeBusinessPatterns.txt")
+        .withColumn("zip", lpad(col("zip"), 5, "0"))
+        .withColumn("est", col("est").cast(DoubleType))
+        .filter(col("naics") === targetNaics)
 
     val popIncDF = spark.read
-        .option("header", true)
+        .option("header", value = true)
         .csv("../Data/zip_population_income_2022.csv")
         .withColumnRenamed("ZCTA20", "zip")
-        .withColumn("TOTPOP", $"TOTPOP".cast(DoubleType))
-        .withColumn("MEDFAMINC", $"MEDFAMINC".cast(DoubleType))
+        .withColumn("TOTPOP", col("TOTPOP").cast(DoubleType))
+        .withColumn("MEDFAMINC", col("MEDFAMINC").cast(DoubleType))
 
-    //join on zip and divide by pop
+    println("Business rows: " + businessDF.count())
+    println("Census rows: " + popIncDF.count())
+
     val joinedDF = businessDF
-        .join(censusDF, Seq("zip"))
-        .filter($"TOTPOP" > 0)
+        .join(popIncDF, Seq("zip"))
+        .filter(col("TOTPOP") > 0)
 
     val featuresDF = joinedDF
-      .withColumn("est_per_capita", $"est" / $"TOTPOP")
+      .withColumn("est_per_capita", col("est") / col("TOTPOP"))
       .select("zip", "stabbr", "cty_name", "est_per_capita", "MEDFAMINC")
+        val cleanDF = featuresDF.na.drop(Array("MEDFAMINC", "est_per_capita"))
 
-    featuresDF.show(10, truncate = false)
+        println("Number of rows in DataFrame: " + cleanDF.count())
+        println("Schema of DataFrame:")
+        cleanDF.printSchema()
 
-    spark.stop()
-    
+try {
+
+    val csvData = cleanDF.collect()
+      .map(_.mkString(","))
+      .mkString("\n")
+
+    val header = cleanDF.columns.mkString(",")
+    val fullData = header + "\n" + csvData
+
+    import java.io.{File, PrintWriter}
+    val writer = new PrintWriter(new File("src/main/resources/features.csv"))
+    writer.write(fullData)
+    writer.close()
+
+    println("File successfully written to features.csv")
+
+
+} catch {
+    case e: Exception =>
+    println("Error writing to file:" + e.getMessage)
+    e.printStackTrace()
+}
+        spark.stop()
     }
 }
